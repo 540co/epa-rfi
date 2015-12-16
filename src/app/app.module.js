8152585cacd118f5988afaad942c6a087188eae8
Jon Minter <jon@540.co>
2015-12-16 11:25:08 -0500

    Started working on Data Service for AirHound app #13

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('app', ['app.core', 'app.layout', 'app.dashboard']);
+    .module('app', ['app.core', 'app.layout', 'app.services', 'app.dashboard']);
 
 })();
